796d16f368a18e5f58096c54302437c4656fdff9
Dominick Triola <dominick@ignovadesign.com>
2018-07-15 21:21:27 -0700

    Refactor header into its own section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './sections/Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo App-logo-1" alt="logo" />
-          <img src={logo} className="App-logo App-logo-2" alt="logo" />
-          <h1 className="App-title">Dominick Triola</h1>
-        </header>
+        <Header />
       </div>
     );
   }
---
+++ src/sections/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../logo.svg';
+
+export default () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo App-logo-1" alt="logo" />
+    <img src={logo} className="App-logo App-logo-2" alt="logo" />
+    <h1 className="App-title">Dominick Triola</h1>
+  </header>
+);
