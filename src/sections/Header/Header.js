1e0d505a140b6b8a81c1a2c4fdaf00fa5f6a0fc9
Dominick Triola <dominick@ignovadesign.com>
2018-07-22 10:21:27 -0700

    Refactor Header styles to use BEM

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Header.css';
 import logo from '../../logo.svg';
 
 export default () => (
-  <header className="App-header">
-    <img src={logo} className="App-logo App-logo-1" alt="logo" />
-    <img src={logo} className="App-logo App-logo-2" alt="logo" />
-    <h1 className="App-title">Dominick Triola</h1>
+  <header className="Header">
+    <img src={logo} className="Header__logo Header__logo--spin-clockwise" alt="logo" />
+    <img src={logo} className="Header__logo Header__logo--spin-counterclockwise" alt="logo" />
+    <h1 className="Header__title">Dominick Triola</h1>
   </header>
 );
